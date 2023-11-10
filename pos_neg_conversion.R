0faf7baa4115980f91aa75f6b28f828e3a6a2ce7
LavakauT <132649549+LavakauT@users.noreply.github.com>
2023-11-10 10:43:04 +0800

    Update pos_neg_conversion.R

--- pos_neg_conversion.R
+++ pos_neg_conversion.R
@@ -4,19 +4,32 @@ library(dplyr)
 library(stringr)
 
 
-dir <- 'path/to/output/downsizing/negative/gene/fasta/folder/'
+dir <- '/path/to/all/kmers/folder'
+folder <- list.dirs(dir,
+                    full.names = FALSE,
+                    recursive = FALSE)
 middle <- 'neg'
-ends <- '.txt.fa_pcre_df_p0.01.txt'
+ends <- '.txt.fa.pcre_df_p0.01.txt'
 
-for ( i in 1:10){
-  number <- i
-  df <- read.delim(paste0(dir, paste(middle, number, sep = '_'), ends))
-  df$Class <- str_replace_all(df$Class, '1', 'pos') # class 1 replace with postive
-  df$Class <- str_replace_all(df$Class, '0', 'neg')# class 0 replace with negative
-  
-  # output result
-  write.table(df,
-              paste0(dir, paste(middle, number, sep = '_'), ends)),
-  row.names = F,
-  quote = F,
-  sep = '\t'}
+
+
+for (j in 1:length(folder)) {
+  folder_name <- folder[j]
+  for ( i in 1:10){
+    number <- i
+    df <- read.delim(paste0(dir, '/', folder_name, '/',
+                            paste(middle, folder_name, number, sep = '_'), ends))
+    df$Class <- str_replace_all(df$Class, '1', 'pos') # class 1 replace with postive
+    df$Class <- str_replace_all(df$Class, '0', 'neg')# class 0 replace with negative
+    
+    # output result
+    write.table(df, paste0(dir, '/', folder_name, '/',
+                       paste(middle, folder_name, number, sep = '_'), ends),
+    row.names = F,
+    quote = F,
+    sep = '\t')
+    
+    }
+}
+
+
